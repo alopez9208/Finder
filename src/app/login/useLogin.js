ae0052e44c9928142a1f54914d9d3b865df4de13
alopez9208 <alopezg9208@gmail.com>
2025-05-10 12:29:28 -0500

    separar la logica de js

---
+++ src/app/login/useLogin.js
@@ -0,0 +1,52 @@
+'use client'
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const useLogin = () => {
+
+    const [usuario, setUsuario] = useState("");
+    const [contrasena, setContrasena] = useState("");
+    const [mostrarContrasena, setMostrarContrasena] = useState(false);
+  
+  
+    const router = useRouter();
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log("✅ Se hizo clic en Ingresar");
+  
+      try {
+        const res = await fetch("/api/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ pkusuario: usuario, contrasena: contrasena }),
+        });
+  
+        const data = await res.json();
+        console.log("🔄 Respuesta del servidor:", data); 
+  
+        if (data.success) {
+          window.location.href = "/dashboard";
+        } else {
+          alert(data.message);
+        }
+      } catch (error) {
+        console.error("❌ Error en el fetch:", error);
+        alert("Error al conectarse con el servidor.");
+      }
+    };
+    
+    return {
+        handleSubmit,
+        setUsuario,
+        setContrasena,
+        usuario,
+        contrasena,
+        mostrarContrasena,
+        setMostrarContrasena,
+        router
+    }
+}
+
+export default useLogin;
